12df51309cea11f9a1998fb70af7aefd438ba5af
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-21 16:09:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,3 +21,18 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
