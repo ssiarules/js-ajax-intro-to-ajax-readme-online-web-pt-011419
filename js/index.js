b1920ed2643297beffcba7a1819ff53b97398c17
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-21 16:02:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 // your code here
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 
